a362422487ea2816b58cdb6549f4b92506e2de88
matt knox <matthewknox@gmail.com>
2010-03-22 18:40:33 -0700

    improved contributors.sh to include files.

--- contributors.sh
+++ contributors.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
+echo "number of lines"
 find . -type f | egrep -v "\.git" | xargs -n 1 git blame | perl -p -e 's/^[^(]+\(([^0-9]+).+/$1/' | uniq -c | sort -r
+
+echo "\nnumber of solutions"
+find . -type f | egrep -v "/\.git/" | cut -f 2 -d "_" | grep -v / | cut -f 1 -d "." | uniq -c | sort -r
