de2801e6208808e81707574271f1592ba7d483c5
matt knox <matthewknox@gmail.com>
2010-03-25 17:23:50 -0700

    moved files to correct places.

--- 41_mknox.scm
+++
@@ -1,5 +0,0 @@
-(include (file "/Users/mknox/h/twitter_sicp/1/mknox.scm"))
-(define (double f)
-  (lambda (x) (f (f x))))
-
-(((double (double double)) inc) 5)
--- 42_mknox.scm
+++
@@ -1,1 +0,0 @@
-(define (compose f g) (lambda (x) (f (g x))))
