43e8e57f7ec04590838d1b655f2566f91aaccfbd
Steve Jenson <stevej@pobox.com>
2010-04-07 11:05:48 -0700

    the compiler gets confused by the precedence.

--- 2/6_stevej.hs
+++ 2/6_stevej.hs
@@ -2,5 +2,5 @@ zero = \x -> x
 one  = \x -> \x -> x
 two  = \x -> \x -> \x -> x
 
-add m n  = \f -> \x -> (m f) . (n f) x
-mult m n = \f -> \x -> (m f) $ (n f) x
+add  m n = \f -> \x -> (m f) . (n f) $ x
+mult m n = \f -> \x -> (m f) $ (n f) $ x
