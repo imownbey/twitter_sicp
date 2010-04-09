0f1cac8baa8b68a0d2f6d1c1fb747318c5a0d039
Jane Huang <jane@twitter.com>
2010-04-08 21:13:27 -0700

    56

--- 2/56_janedotx.scm
+++ 2/56_janedotx.scm
@@ -12,15 +12,19 @@
            (make-product (deriv (multiplier ex) var)
                          (multiplicand ex))))
         ((exponentiation? ex)
-            (make-exponentiation (deriv ())
-
+         (make-product (exponent ex) (make-exponentiation (base ex) (- (exponent ex) 1)))) 
         (else
          (error "unknown exression type -- DERIV" ex))))
 
 (define (exponentiation? x) 
-  (and (pair? x) (eq? (car x) 'ext)))
+  (and (pair? x) (eq? (car x) 'expt)))
 
-(define (make-exponentiation ex)
+(define (make-exponentiation a1 a2)
+  (cond ((=number? a1 0) 0)
+        ((=number? a2 0) 1)
+        ((=number? a2 1) a1)
+        ((and (number? a1) (number? a2)) (expt a1 a2))
+        (else (list 'expt a1 a2))))
 
 (define (base x)
   (cadr x))
@@ -61,4 +65,10 @@
         ((=number? m2 1) m1)
         ((and (number? m1) (number? m2)) (* m1 m2))
         (else (list '* m1 m2))))
+;; does the right thing
+(deriv '(expt x 2) 'x)
+
+;;doesn't quite simplify it, but w/ever
+(deriv '(expt (* 2 x) 5) 'x)
+
 
