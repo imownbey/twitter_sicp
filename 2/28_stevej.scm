961e427174a1471d4e5a37e252bc7be136b038c8
Steve Jenson <stevej@pobox.com>
2010-04-11 15:10:01 -0700

    renaming it since it's not completely iterative.

--- 2/28_stevej.scm
+++ 2/28_stevej.scm
@@ -1,12 +1,15 @@
 (define x (list (list 1 2) (list 3 4)))
 
 (define (fringe list)
-  (define (fringe-iter list results)
+  (define (fringe-inner list results)
     (cond ((null? list) results)
           ((pair? (car list))
-           (fringe-iter (cdr list) (fringe-iter (car list) results)))
-          (else (fringe-iter (cdr list) (append results (cons (car list) '()))))))
-  (fringe-iter list '()))
+           (fringe-inner (cdr list) (fringe-inner (car list) results)))
+          (else (fringe-inner (cdr list) (append results (cons (car list) '()))))))
+  (fringe-inner list '()))
+
+;; making it clear what we've built here.
+(define flatten fringe)
 
 (equal? '(1 2 3 4) (fringe x))
 (equal? '(1 2 3 4 1 2 3 4) (fringe (list x x)))
